150cb4101b10c0f985f49f9e9a8676d694d94f61
spidunno <unnospid@gmail.com>
2025-03-15 12:36:54 -0400

    update CI

--- src/App.tsx
+++ src/App.tsx
@@ -2,36 +2,61 @@ import { createSignal } from "solid-js";
 import MapGL, { Layer, Source, Viewport } from "solid-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 
+import mbxClient from "@mapbox/mapbox-sdk";
+import mbxDatasets from "@mapbox/mapbox-sdk/services/datasets";
+
+const datasetId = "cm8aecomp0iat1pph34esnelo";
+
+const accessToken = `pk.eyJ1Ijoic3BpZHVubm8iLCJhIjoiY2x3ZHlmdmd4MTl6ODJqcnM1a3AwNG9zZiJ9.vqd5rqsIKuDzdBUnWqPU1g`;
+const notImportant = `sk.eyJ1Ijoic3BpZHVubm8iLCJhIjoiY204YWVoaXo5MWFtODJqb21xMm5pMnN1NiJ9.yqR1ftjPhaeMQUood-xBKg`;
+
+const mapboxClient = mbxClient({ accessToken: notImportant });
+const datasetsService = mbxDatasets(mapboxClient);
+
+type Position = [number, number];
+
+async function addPoint(coordinates: Position) {
+	return await datasetsService
+		.putFeature({
+			datasetId,
+			feature: {
+				geometry: {
+					coordinates,
+					type: "Point",
+				},
+				type: "Feature",
+				properties: {},
+			},
+			featureId: `${coordinates[0]}_${coordinates[1]}`,
+		})
+		.send();
+}
+
+// type DatasetObject = {
+//   "geometry": {
+//     "coordinates": [
+//       number,
+//       number
+//     ],
+//     "type": "Point"
+//   },
+//   "type": "Feature"
+// }
+
 export default function App() {
-  const [viewport, setViewport] = createSignal({
-    center: [-122.45, 37.78],
-    zoom: 11,
-  } as Viewport);
+	const [viewport, setViewport] = createSignal({
+		center: [-122.45, 37.78],
+		zoom: 11,
+	} as Viewport);
 
-
-  return (
-    <MapGL
-      options={{ accessToken: "pk.eyJ1Ijoic3BpZHVubm8iLCJhIjoiY2x3YXZtMGt5MGkyajJwdHhrMTZ4NGx1MSJ9.u2FfwTZYNI2Q4LAni2BSsg" }}
-      viewport={viewport()}
-      onViewportChange={(evt: Viewport) => setViewport(evt)}
-      
-    >
-      <Source
-        source={{
-          type: "geojson",
-          data: "https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson",
-        }}
-      >
-        <Layer
-          style={{
-            type: "circle",
-            paint: {
-              "circle-radius": 8,
-              "circle-color": "red",
-            },
-          }}
-        />
-      </Source>
-    </MapGL>
-  );
+	return (
+		<div style={{ margin: "0", padding: "0", display: "flex", "flex-direction": "row" }}>
+			<MapGL
+				options={{ accessToken: accessToken }}
+				viewport={viewport()}
+				onViewportChange={(evt: Viewport) => setViewport(evt)}
+			></MapGL>
+      <button>Add point</button>
+		</div>
+	);
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig, UserConfig } from 'vite'
 import solid from 'vite-plugin-solid'
 
-export default defineConfig({
-  plugins: [solid()],
-})
+export default defineConfig(({ mode }) => {
+	const common: UserConfig = {
+		plugins: [solid()],
+	};
+	if (mode === "gh-pages") {
+		return { ...common, base: "/dogpiss" };
+	} else {
+		return common;
+	}
+});
